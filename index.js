3f37bd4e10e2664a9ccf0f4b3c0a9f342819f783
Chase <chaseanderson.stu@gmail.com>
2024-02-16 12:47:38 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    const body = JSON.stringify({ name, email });
+  
+    return fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: body
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Find the new id and append it to the DOM
+        const id = data.id;
+        document.body.innerHTML += `<p>New user id: ${id}</p>`;
+      })
+      .catch(error => {
+        // Append the error message to the DOM
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
